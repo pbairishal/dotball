d0670f431364afe5000794857afb862e8b8565ba
Pradosh <pbairishal@gmail.com>
2018-09-02 22:05:10 +0530

    updated from invision design

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,20 +2,6 @@ $(".navbar-link-toggle").click(function() {
     $(".navbar-toggle-show").toggle("flex");
 });
 
-
-var animData = {
-    wrapper: document.getElementById('lottie'),
-    animType: 'svg',
-    loop: true,
-    prerender: true,
-    rendererSettings: {
-        preserveAspectRatio: "xMaxYMax meet",
-    },
-    autoplay: true,
-    path: 'https://cors.io/?https://s3.ap-south-1.amazonaws.com/ipl-2018-webtest/test.json'
-};
-var anim = bodymovin.loadAnimation(animData);
-
 $(window).scroll(function() {
     if ($(window).scrollTop() >= 50) {
         $('.navbar').addClass("header-bg");
